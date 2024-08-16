e01996a9926d3c12c3987d0b574b1f5cf99193dc
“Alexis” <98814794+HubertAlexis@users.noreply.github.com>
2024-08-16 11:32:26 +0200

    push sceneManager

---
+++ TestLens/Assets/Untitled JavaScript.js
@@ -0,0 +1,7 @@
+global.sceneManager.loadSceneSync('2', {
+  // Optional parameters
+  additive: true, // Default value
+  enabled: true, // Default value
+  parent: undefined, // Default value
+});
+
